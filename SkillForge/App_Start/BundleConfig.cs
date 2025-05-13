7e48a660757e4b4a40d75adb2f05c2e53bb24fd7
Danik <daniel.ciob2@gmail.com>
2025-05-13 12:04:57 +0000

    punctul din authentificare facut si functioneaza bine. hero sectio de modificat

--- SkillForge/App_Start/BundleConfig.cs
+++ SkillForge/App_Start/BundleConfig.cs
@@ -6,56 +6,56 @@ using System.Web.Optimization;
 
 namespace SkillForge.App_Start
 {
-     public class BundleConfig
-     {
-          public static void RegisterBundles(BundleCollection bundles)
-          {
-               // Core CSS Bundles
-               bundles.Add(new StyleBundle("~/bundles/core/css").Include(
-                    "~/Content/assets/vendor/css/core.css",
-                    "~/Content/assets/vendor/fonts/iconify-icons.css",
-                    "~/Content/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css",
-                    "~/Content/assets/vendor/libs/apex-charts/apex-charts.css"
-               ));
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Core CSS Bundles
+            bundles.Add(new StyleBundle("~/bundles/core/css").Include(
+                 "~/Content/assets/vendor/css/core.css",
+                 "~/Content/assets/vendor/fonts/iconify-icons.css",
+                 "~/Content/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css",
+                 "~/Content/assets/vendor/libs/apex-charts/apex-charts.css"
+            ));
 
-               // Demo CSS Bundle
-               bundles.Add(new StyleBundle("~/bundles/demo/css").Include(
-                    "~/Content/assets/css/demo.css"
-               ));
+            // Demo CSS Bundle
+            bundles.Add(new StyleBundle("~/bundles/demo/css").Include(
+                 "~/Content/assets/css/demo.css"
+            ));
 
-               // Login CSS Bundle
-               bundles.Add(new StyleBundle("~/bundles/login/css").Include(
-                    "~/Content/assets/vendor/css/pages/page-auth.css"
-               ));
+            // Login CSS Bundle
+            bundles.Add(new StyleBundle("~/bundles/login/css").Include(
+                 "~/Content/assets/vendor/css/pages/page-auth.css"
+            ));
 
-               // Hero/Courses CSS Bundle
-               bundles.Add(new StyleBundle("~/bundles/hero/css").Include(
-                    "Content/assets/vendor/css/pages/hero.css"
-               ));
+            // Hero/Courses CSS Bundle
+            bundles.Add(new StyleBundle("~/bundles/hero/css").Include(
+                 "~/Content/assets/vendor/css/pages/hero.css"
+            ));
 
-               // Core JavaScript Bundles
-               bundles.Add(new ScriptBundle("~/bundles/core/js").Include(
-                    "~/Content/assets/vendor/js/helpers.js",
-                    "~/Content/assets/js/config.js",
-                    "~/Content/assets/vendor/libs/jquery/jquery.js",
-                    "~/Content/assets/vendor/libs/popper/popper.js",
-                    "~/Content/assets/vendor/js/bootstrap.js",
-                    "~/Content/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js",
-                    "~/Content/assets/vendor/js/menu.js",
-                    "~/Content/assets/js/main.js",
-                    "~/Content/assets/vendor/libs/apex-charts/apexcharts.js",
-                    "~/Content/assets/js/dashboards-analytics.js"
-               ));
+            // Core JavaScript Bundles
+            bundles.Add(new ScriptBundle("~/bundles/core/js").Include(
+                 "~/Content/assets/vendor/js/helpers.js",
+                 "~/Content/assets/js/config.js",
+                 "~/Content/assets/vendor/libs/jquery/jquery.js",
+                 "~/Content/assets/vendor/libs/popper/popper.js",
+                 "~/Content/assets/vendor/js/bootstrap.js",
+                 "~/Content/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js",
+                 "~/Content/assets/vendor/js/menu.js",
+                 "~/Content/assets/js/main.js",
+                 "~/Content/assets/vendor/libs/apex-charts/apexcharts.js",
+                 "~/Content/assets/js/dashboards-analytics.js"
+            ));
 
-               // Login JavaScript Bundle
-               bundles.Add(new ScriptBundle("~/bundles/login/js").Include(
-                    "~/Content/assets/js/pages-auth.js"
-               ));
+            // Login JavaScript Bundle
+            bundles.Add(new ScriptBundle("~/bundles/login/js").Include(
+                 "~/Content/assets/js/pages-auth.js"
+            ));
 
-               // Favicon
-               bundles.Add(new StyleBundle("~/bundles/favicon").Include(
-                    "~/Content/assets/img/favicon/head.ico"
-               ));
-          }
-     }
+            // Favicon
+            bundles.Add(new StyleBundle("~/bundles/favicon").Include(
+                 "~/Content/assets/img/favicon/head.ico"
+            ));
+        }
+    }
 }
--- SkillForge/Controllers/LoginController.cs
+++ SkillForge/Controllers/LoginController.cs
@@ -13,13 +13,36 @@ namespace SkillForge.Controllers
         {
             return View();
         }
+
         public ActionResult Auth()
         {
-               return View();
+            return View();
         }
-          public ActionResult ForgotPassword()
-          {
-               return View();
-          }
-     }
+
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            // Add your login logic here
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult Register(string username, string email, string password, string confirmPassword)
+        {
+            // Add your registration logic here
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult ForgotPassword(string email)
+        {
+            // Add your forgot password logic here
+            return RedirectToAction("Login", "Login");
+        }
+    }
 }
